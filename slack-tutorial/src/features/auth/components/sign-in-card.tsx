8923dc0a1dcbbb54de72fde01f4ee065d6f82afc
unknown <nguyenhuuquangmss@gmail.com>
2025-04-02 13:18:07 +0700

    first commit

---
+++ slack-tutorial/src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@radix-ui/react-separator";
+
+export const SignInCard = () => {
+    return (
+        <Card className="w-full h-full p-8">
+            <CardHeader className="px-0 pt-0">
+                <CardTitle>
+                    Log in to continue
+                </CardTitle>
+                <CardDescription>
+                    Use your email or another service to continue
+                </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-5 px-0 pb-0">
+                <form className="space-y-2.5">
+                    <Input
+                        disabled={false}
+                        value=""
+                        onChange={()=>{}}
+                        placeholder="Email"
+                        type="email"
+                        required
+                    />
+                    <Input
+                        disabled={false}
+                        value=""
+                        onChange={()=>{}}
+                        placeholder="Password"
+                        type="password"
+                        required
+                    />
+                    <Button type="submit" className="w-full" size="lg" disabled={false}>
+                        Continue
+                    </Button>
+                </form>
+                <Separator />
+
+            </CardContent>
+        </Card>
+    );
+};
